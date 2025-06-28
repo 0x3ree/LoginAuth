202ae8389f5ecc37d829ea06fa5039d436237940
Three <ottunade95@gmail.com>
2025-06-28 19:53:05 +0100

    Implement login functionality with loading state and refactor authentication methods

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false); // This state is used to show a loading indicator while the user is being created. It is not used in this example but can be used to show a loading overlay/indicator while the user is being created.
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true); // This will set the isAuthenticating state to true when the user submits the signup form.
+    await login(email, password); // This function will be called when the user submits the signup form. It will create a new user using the createUser function from util/auth.js.(this is what links the signup screen to the backend)
+    setIsAuthenticating(false); // This will set the isAuthenticating state to false when the user is created so it doesn't continue spinning.
+  }
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Login User In..."} />; // the message prop is gotten from the loading overlay function
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -2,15 +2,27 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyAAuSEIF0ILOmbX0ViqYuqCv-km_nHgSLA"; // this is our web api key for Firebase Authentication which stored in the const.
 
-// This is the API key for Firebase Authentication, which is used to authenticate requests to the Firebase Authentication service.
-// when we trigger the createUser fucntion, it will send a post request to FB automatically a promise comes in and we make our post request an async fucntion and extracts its response using await and then we can use the response to do whatever we want with it, like storing the user in the database or logging them in.
-export async function createUser(email, password) {
+// due to the similarities between the signup and sign in end point we'd save most of the part in a const and then create diff functions for where they differ in url
+// the MODE(signIN or signUP))
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`; // this is the endpoint for Firebase Authentication, where mode can be either signUp or signInWithPassword, we simply broke it down
   const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    url,
     {
       email: email,
       password: password,
       returnSecureToken: true, // this is used to return a secure token for the user
-    } // this is the endpoint for creating a new user in Firebase Authentication (this(email,pass, RST) should always be in an object format which will be sent as the body of the request which will be converted to JSON(a data format) by axios)
+    } // this is the endpoint for creating a new user in Firebase Authentication (this(email,pass, RST) should always be in an object format which will be sent as the body of the request which will be converted to JSON(a data format) by axios));
   );
+  console.log(response.data);
+}
+
+// This is the API key for Firebase Authentication, which is used to authenticate requests to the Firebase Authentication service.
+// when we trigger the createUser fucntion, it will send a post request to FB automatically a promise comes in and we make our post request an async fucntion and extracts its response using await and then we can use the response to do whatever we want with it, like storing the user in the database or logging them in.
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password); // this will call the authenticate function with the signUp mode and the email and password provided by the user
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password); // this will call the authenticate function with the signInWithPassword mode and the email and password provided by the user
 }
